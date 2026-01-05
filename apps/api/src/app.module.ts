0f108aa45ec2a143ad504756478eecb9426e2bd1
jgiral-metacodi <jgiral73@gmail.com>
2026-01-05 14:33:43 +0100

    install @nestjs/config

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // NOTA: Amb això ara els arxius d'environment funcionen dins a l'api (ex: "process.env.dev")
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
